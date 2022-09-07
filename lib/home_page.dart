88dfc9ffd8da2431cc42663c4e819cc892eb3ae9
Christan Shane Plaza <christan.shane.plaza@sun-asterisk.com>
2022-09-07 21:11:57 +0800

    Added commands

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,8 +9,49 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // Reference our box
+  final _myBox = Hive.box('mybox');
+
+  // Write Data
+  void writeData() {
+    _myBox.put(1, 'Tantan');
+  }
+
+  // Read Data
+  void readData() {
+    print(_myBox.get(1));
+  }
+
+  // Delete Data
+  void deleteData() {
+    _myBox.delete(1);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            MaterialButton(
+              onPressed: writeData,
+              child: Text('Write'),
+              color: Colors.green,
+            ),
+            MaterialButton(
+              onPressed: readData,
+              child: Text('Read'),
+              color: Colors.blue,
+            ),
+            MaterialButton(
+              onPressed: deleteData,
+              child: Text('Delete'),
+              color: Colors.red,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
